021797ac292bcfa475a5fe63663c52bced426f01
Artur Taranchiev <github@in.ewok.email>
2023-01-08 01:39:02 +0600

    Day 4

--- src/aoc2022/core.clj
+++ src/aoc2022/core.clj
@@ -2,10 +2,13 @@
   (:gen-class)
   (:require [aoc2022.day1 :as day1]
             [aoc2022.day2 :as day2]
-            [aoc2022.day3 :as day3]))
+            [aoc2022.day3 :as day3]
+            [aoc2022.day4 :as day4]))
+
 
 (defn -main []
   (println (map #(format "Day %s result: %s\n" (+ 1 (first %)) (second %))
                 (map-indexed vector [(day1/do)
                                      (day2/do)
-                                     (day3/do)]))))
+                                     (day3/do)
+                                     (day4/do)]))))
---
+++ src/aoc2022/day4.clj
@@ -0,0 +1,39 @@
+(ns aoc2022.day4
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]))
+
+(def input
+  (->> "day4_input.txt"
+       io/resource
+       io/file
+       slurp
+       string/split-lines
+       (map #(string/split % #","))
+       (map #(let [f (first %) s (second %)]
+               [(string/split f #"-")
+                (string/split s #"-")]))))
+
+(defn overlapped? [pair]
+  (let [ff (Integer/parseInt (ffirst pair))
+        fs (Integer/parseInt (second (first pair)))
+        sf (Integer/parseInt (first (second pair)))
+        ss (Integer/parseInt (second (second pair)))]
+    (cond
+      (and (>= ff sf)
+           (<= fs ss)) 1
+      (and (<= ff sf)
+           (>= fs ss)) 1
+      (and (>= ff sf)
+           (<= ff ss)) 2
+      (and (>= fs sf)
+           (<= fs ss)) 2
+      :else 0)))
+
+(def how-many-overlapped-fully
+  (reduce + (map #(if (= (overlapped? %) 1) 1 0) input)))
+
+(def how-many-overlapped-partly
+  (reduce + (map #(if (>= (overlapped? %) 1) 1 0) input)))
+
+(defn do []
+  [how-many-overlapped-fully how-many-overlapped-partly])
